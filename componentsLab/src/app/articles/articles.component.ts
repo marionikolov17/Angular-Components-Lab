7b2d2934cd1d6433356a47a4e721dbde9f52b21f
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-02-28 12:47:59 +0200

    Import ready Articles Component into App

--- componentsLab/src/app/articles/articles.component.ts
+++ componentsLab/src/app/articles/articles.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ArticleData } from 'src/data';
+import { Article } from 'src/models/article.model';
 
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.css']
 })
-export class ArticlesComponent {
+export class ArticlesComponent implements OnInit {
+  articles: Article[];
 
+  constructor() {
+    this.articles = [];
+  }
+
+  ngOnInit(): void {
+      this.articles = new ArticleData().getData();
+  }
 }
